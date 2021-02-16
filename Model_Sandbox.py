4ca0463ddad3142d4e5294a05d5da1084f7669a7
Nyle Siddiqui <siddiqun8701@uwec.edu>
2021-02-15 22:04:35 -0600

    2/15 checkpoint - intro screen and model start

--- Model_Sandbox.py
+++ Model_Sandbox.py
@@ -2,18 +2,27 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 import matplotlib as plt
-# from tensorflow import keras
-# from tensorflow.keras import layers, Sequential
+from tensorflow import keras
+from tensorflow.keras import layers, Sequential
 from sklearn.model_selection import train_test_split
 
 
 
 if __name__ == '__main__':
-	df = pd.read_csv("Subject0.csv", delimiter=';', skiprows=1)
-	print(df.head())
-	X = df.iloc[1:3]
-	# y = df.loc[:, 1]
-	print("X: {}".format(X))
-	# print(y)
-	# X_train, X_test, y_train, y_test = train_test_split()
+	df = pd.read_csv("Subject0.txt", delimiter=';')
+	X = df.iloc[:, 0:len(df.columns) - 1]
+	y = df.iloc[:, len(df.columns) - 1]
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.30)
+	print(X_train)
+
+	model = keras.Sequential(
+		[
+			layers.Dense(20, input_dim=4, activation="relu"),
+			
+			
+			
+			layers.Dense(35, activation='Softmax')
+			
+		]
+	)
 	
--- Mouse Tracker with Pynput.py
+++ Mouse Tracker with Pynput.py
@@ -1,18 +1,22 @@
 from pynput import mouse
-import PySimpleGUI as gui
+import pygame
 import sys
 import time
 global start
 global start_location
 global start_switch
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+global count
 
 def on_move(x, y):
+	global count
 	global start_switch
 	global start_location
 	if start_switch == 0:
 		start_location = (x, y)
 		start_switch += 1
-	print((x, y), -1, -1, (start_location[0] - x, start_location[1] - y))
+	print((x, y), ';', -1, ';', -1, ';', (start_location[0] - x, start_location[1] - y), ';', 0)
 
 def on_click(x, y, button, pressed):
 	global start
@@ -20,19 +24,44 @@ def on_click(x, y, button, pressed):
 		pass
 	else:
 		end = time.time()
-		print((x, y), button, end-start, (start_location[0] - x, start_location[1] - y))
+		formatted_time = "{:.5f}".format(end-start)
+		print((x, y), ';', button, ';', formatted_time, ';', (start_location[0] - x, start_location[1] - y), ';', 0)
 		start = time.time()
 
 
 def on_scroll(x, y, dx, dy):
 	if dy > 0:
-		print((x, y), "up", -1, (-1, -1))
+		print((x, y), ';', "scroll up", ';', -1, ';', (-1, -1),';', 0)
 	else:
-		print((x, y), "down", -1, (-1, -1))
-
+		print((x, y), ';', "scroll down", ';', -1, ';', (-1, -1),';', 0)
+		
+def start_screen():
+	running = True
+	pygame.init()
+	while running:
+		screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+		font = pygame.font.SysFont('Arial', 25)
+		text = font.render('Click \'START\' to begin', True, (255, 255, 255), (0, 0, 0))
+		text2 = font.render('START', True, (255, 255, 255), (0, 0, 0))
+		textRect2 = text2.get_rect()
+		textRect = text.get_rect()
+		textRect.center = (SCREEN_WIDTH//2, SCREEN_HEIGHT//4)
+		textRect2.center = (SCREEN_WIDTH//2, SCREEN_HEIGHT//1.5)
+		screen.fill((0, 0, 0))
+		screen.blit(text, textRect)
+		screen.blit(text2, textRect2)
+		pygame.display.update()
+		event = pygame.event.wait()
+		if event.type == pygame.QUIT:
+			running = False
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			running = False
+			pygame.quit()
 if __name__ == '__main__':
-	# sys.stdout = open("Subject0.txt", 'w')
-	gui.Window(title="Mouse Tracker", layout=[[]], margins=(300, 250)).read()
+	count = 0
+	start_screen()
+	sys.stdout = open("Subject0.txt", 'w')
+	print("(X, Y); Button Pressed; Time; Distance From Start Point; Subject ID")
 	start_switch = 0
 	start = time.time()
 	now = time.time()
@@ -45,5 +74,5 @@ if __name__ == '__main__':
 	while time.time() < future:
 		continue
 	listener.stop()
-	# sys.stdout.close()
+	sys.stdout.close()
 
